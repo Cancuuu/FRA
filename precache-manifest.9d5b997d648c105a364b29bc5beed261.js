295e7221e50d6301be185cb610f46745acaa065e
Cancu <juancruz.cancu@gmail.com>
2020-08-28 01:19:13 -0300

    Updates

---
+++ precache-manifest.9d5b997d648c105a364b29bc5beed261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e736d7f4bb62035874be322d7fc625",
+    "url": "/FRA/index.html"
+  },
+  {
+    "revision": "7b8b497b45f3b1896359",
+    "url": "/FRA/static/css/main.4c3b0ccb.chunk.css"
+  },
+  {
+    "revision": "bed22d91fde67ed3f020",
+    "url": "/FRA/static/js/2.1de8db21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FRA/static/js/2.1de8db21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8b497b45f3b1896359",
+    "url": "/FRA/static/js/main.c4d94136.chunk.js"
+  },
+  {
+    "revision": "514e5dfbfb7ce825821f",
+    "url": "/FRA/static/js/runtime-main.6fe1bc04.js"
+  }
+]);
